2c4ce9e740e97e39abb6fa775ec9dc64e2848458
ThomasLe <88428250+ThomasLe-dev@users.noreply.github.com>
2023-03-15 02:16:39 +0700

    create sub module

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from "react-router-dom";
 
-//Utils
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 
-//Component
-import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
 
-//Service Worker
-import * as serviceWorker from "./serviceWorker";
+    <BrowserRouter>
 
-ReactDOM.render(
-	<Router>
-		<App />
-	</Router>,
-	document.getElementById("root")
+      <ChakraProvider>
+        
+        <App />
+        
+      </ChakraProvider>
+
+    </BrowserRouter>
+
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
